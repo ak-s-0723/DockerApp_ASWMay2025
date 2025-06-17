07bec3d08bf75f4550467248479d0a5712562ed6
khannaanurag1 <khannaanurag1@gmail.com>
2025-06-17 09:41:21 +0530

    docker demo

---
+++ src/main/java/org/example/dockerapplication_aswmay2025/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.dockerapplication_aswmay2025.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/{key}")
+    public String welcome(@PathVariable("key") String key) {
+        return "Hello "+key;
+    }
+}
